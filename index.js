8f6e8ec837b569681014707fd131b7db442f15f9
Jeff Butler <jeff.butler.springdale@gmail.com>
2019-09-16 01:30:32 -0500

    mvp

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const bcrypt = require('bcryptjs')
+
+const db = require('./database/dbConfig.js');
+const Users = require('./users/users-model.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+  res.send("It's alive!");
+});
+
+server.post('/api/register', (req, res) => {
+    let user = req.body;
+    console.log('password arriving from client', user.password)
+    user.password = bcrypt.hashSync(user.password, 10); //running through the hash a certain amount of times
+    console.log('password heading to db', user.password)
+    Users.add(user)
+      .then(saved => {
+        res.status(201).json(saved);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+  
+  server.post('/api/login', (req, res) => {
+    let { username, password } = req.body;
+  
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          res.status(200).json({ message: `Welcome ${user.username}!` });
+        } else {
+          res.status(401).json({ message: 'Invalid Credentials' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+  
+  
+  //we are going to make this protected
+  server.get('/api/users', restricted, (req, res) => {
+    Users.find()
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => res.send(err));
+  });
+  
+  function restricted(req, res, next){
+    const {username, password } = req.headers;
+  
+    if (username && password) {
+      Users.findBy({ username }) 
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        } else {
+          res.status(400).json({message: "invalid credentials"})
+        }
+      })
+      .catch(err => {
+        res.status(500).json({message: "unexpected error"})
+      })
+    } else {
+      res.status(400).json({message: "please provide username and passsword"})
+    }
+  }
+  
+
+const port = process.env.PORT || 6000;
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
